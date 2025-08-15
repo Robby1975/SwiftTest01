5ace4a0ed69e9fb979ecd494a27d920b9636f492
Robert Schorsch <robert.schorsch@itsrs.de>
2025-08-15 13:38:03 +0200

    Initial Commit

---
+++ SwiftTest01Tests/SwiftTest01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTest01Tests.swift
+//  SwiftTest01Tests
+//
+//  Created by Robert Schorsch on 15.08.25.
+//
+
+import Testing
+@testable import SwiftTest01
+
+struct SwiftTest01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
